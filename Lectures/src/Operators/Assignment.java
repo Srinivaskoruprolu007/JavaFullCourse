9e7a57a14a22d5d84b42c6dc5c1c6b29d7f68b67
Srinivaskoruprolu007 <srinivasg3112@gmail.com>
2022-10-08 18:30:07 +0530

    third commit

--- Lectures/src/Operators/Assignment.java
+++ Lectures/src/Operators/Assignment.java
@@ -1,5 +1,22 @@
 package Operators;
 
 public class Assignment {
+    public static void main(String[] args) {
+//               Basically Assignment operators are two types
+//        1. Basic assignment:  operator which is used to assign a value to a variable
+//        2. Compound assignment : operator having combination of arithmetic and assignment
 
+        int b = 34;  // Basic Assignment
+        System.out.println(b);
+        b += 20;  //b = b+20
+        System.out.println(b);
+        b -= 20;  //b = b-20
+        System.out.println(b);
+        b *= 5;  // b = b * 5
+        System.out.println(b);
+        b /= 5;  // b = b/5
+        System.out.println(b);
+        b %= 5;  // b = b%5
+        System.out.println(b);
+    }
 }
